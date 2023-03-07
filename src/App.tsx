cc66546bac87fff2dceeaed4a87ea5409220130a
Daniel Rocha <dgustavo@id.uff.br>
2023-03-07 16:32:28 -0300

    feat: add initial home screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './app.css'
 
 const App = () => {
+  const [showGame, setShowGame] = useState<boolean>(false)
+  const [repos, setRepos] = useState([1])
+
+  
+  const handleStart = (params: any) => {
+    setShowGame(true)
+    console.log('first')
+    return null
+  }
   return (
-    <div>App</div>
+    <main>
+      {!showGame && (
+        <>
+          <h1>Welcome to RepoWars</h1>
+          <h2>A developer-driven guessing game</h2>
+          {repos.length === 0 ? 
+            <div>Loading...</div> : 
+            <button 
+            type="submit"
+            onClick={handleStart}>Start</button>
+          }
+          
+        </>
+      )}
+
+      {showGame && (
+        <>
+          <div>Carregado</div>
+        </>
+      )}
+    </main>
   )
 }
 
